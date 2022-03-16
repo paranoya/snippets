abd6f9639971928a63c0e8145e3caa67f479dc03
Yago Ascasibar <yago.ascasibar@uam.es>
2022-03-16 14:47:53 +0100

    Include statistical weights

--- mode.py
+++ mode.py
@@ -20,7 +20,7 @@ except:
 from matplotlib import pyplot as plt
 
 
-def adaptive_histogram(data, bandwidth=None):
+def adaptive_histogram(data, bandwidth=None, weights=None):
     """
     Compute histogram based on a top-hat kernel with the specified (adaptive) bandwidth.
     :param data: Collection of data points
@@ -32,14 +32,19 @@ def adaptive_histogram(data, bandwidth=None):
         h = 1 + int(np.sqrt(h1 * h2))
     else:
         h = bandwidth
-    x = np.sort(data.flatten())
-    x_cumul = np.cumsum(x)
+    if weights is None:
+        weights = np.ones_like(data)
+
+    sorted_data = np.argsort(data.flatten())
+    x = data.flatten()[sorted_data]
+    w = weights.flatten()[sorted_data]
+    cumul_mass = np.cumsum(w)
 
     x_mid = (x[h:] + x[:-h]) / 2
     # x_mean = (x_cumul[h:] - x_cumul[:-h]) / h
     half_h = int(h/2)
     x_median = x[half_h:half_h-h]
-    density = h / (x[h:] - x[:-h])
+    density = (cumul_mass[h:] - cumul_mass[:-h]) / (x[h:] - x[:-h])
     x_bin = np.sqrt(x_mid*x_median)
     return x_bin, density / data.size
 
@@ -62,7 +67,7 @@ def adaptive_histogram(data, bandwidth=None):
 #     if (mu - estimated_mean) ** 2 < .01 * estimated_variance / total_weight:
 #         break
 
-def find_mode(x):
+def find_mode(x, weights=None):
     """
     Locate the mode of a distribution by fitting a polynomial within +-one-sigma interval.
     :param x: Collection of data points
@@ -75,21 +80,30 @@ def find_mode(x):
     # def L4(x): return (35*x**4 - 30*x**2 + 3) / 8
     def norm_L(n): return 2 / (2 * n + 1)
 
-    x0 = np.nanmean(x)
-    sigma = np.nanstd(x)
+    if weights is None:
+        weights = np.ones_like(x)
+
+    total_mass = np.nansum(weights)
+    x0 = np.nansum(x*weights) / total_mass
+    sigma = np.sqrt(np.nansum((x-x0)**2 * weights)/total_mass)
     delta_peak = 0
     i = 0
     while True:
         i += 1
         x0 += delta_peak
         delta = x - x0
+        w = weights[np.abs(delta) < sigma]
         delta = delta[np.abs(delta) < sigma] / sigma
         # scalar product:
         c0 = np.mean(L0(delta)) / norm_L(0) # point density · L0
-        c1 = np.mean(L1(delta)) / norm_L(1)  # point density · P1
-        c2 = np.mean(L2(delta)) / norm_L(2)  # point density · P2
-        # c3 = np.mean(L3(delta)) / norm_L(3)  # point density · P3
-        # c4 = np.mean(L4(delta)) / norm_L(4)  # point density · P4
+        c1 = np.mean(L1(delta)) / norm_L(1)  # point density · L1
+        c2 = np.mean(L2(delta)) / norm_L(2)  # point density · L2
+        # c3 = np.mean(L3(delta)) / norm_L(3)  # point density · L3
+        # c4 = np.mean(L4(delta)) / norm_L(4)  # point density · L4
+        total_mass = np.nansum(w)
+        c0 = np.nansum(w*L0(delta))/total_mass / norm_L(0) # point density · L0
+        c1 = np.nansum(w*L1(delta))/total_mass / norm_L(1) # point density · L1
+        c2 = np.nansum(w*L2(delta))/total_mass / norm_L(2) # point density · L2
         polynomial_fit = c0*L0(delta) + c1*L1(delta) + c2*L2(delta)  # + c3*L3(delta) + c4*L4(delta)
         delta_peak = delta[np.argmax(polynomial_fit)] * sigma/np.sqrt(i)  # divide by sqrt(i) to prevent oscillations
         # print(x0, delta_peak)
@@ -97,68 +111,87 @@ def find_mode(x):
             return x0, x0+delta*sigma, polynomial_fit*delta.size/x.size/sigma
 
 
-# %% Generate data
+if __name__ == "__main__":
+    # %% Generate data
 
-N = 10000
+    N = 10000
 
-D = 2
-distribution = 'Gaussian in {} dimensions'.format(D)
-x = 0
-for d in range(D):
-    x += np.random.normal(0., 1., N)**2
-x = np.sqrt(x)
-sorted_x = np.sort(x)
-true_density = sorted_x**(D-1) * np.exp(-.5 * sorted_x**2) *2**(1-D/2)/gamma(D/2)
-true_mode = np.sqrt(D-1)
+    D = 2
+    distribution = 'Gaussian in {} dimensions'.format(D)
+    x = 0
+    for d in range(D):
+        x += np.random.normal(0., 1., N)**2
+    x = np.sqrt(x)
+    sorted_by_x = np.argsort(x)
+    sorted_x = x[sorted_by_x]
+    true_density = sorted_x**(D-1) * np.exp(-.5 * sorted_x**2) *2**(1-D/2)/gamma(D/2)
+    true_mode = np.sqrt(D-1)
 
-# %% Find mode
+    statistical_weight = 1/np.sqrt(x)
+    sorted_weight = statistical_weight[sorted_by_x]
+    cumulative_mass = np.cumsum(sorted_weight)
+    true_density *= sorted_weight
 
-x0, x_fit, polynomial_fit = find_mode(x)
-print("Mode = {:.3f} (true = {:.3f})".format(x0, true_mode))
+    # %% Find mode
 
-# %% Identify outliers
+    x0, x_fit, polynomial_fit = find_mode(x, weights=statistical_weight)
+    print("Mode = {:.3f} (true = {:.3f})".format(x0, true_mode))
 
-N_mode = np.searchsorted(sorted_x, x0)
-N_normal = 2*N_mode
-if N_normal < N:
-    print('High values are unusual:')
-    threshold = sorted_x[N_normal]
-    contamination = np.searchsorted(sorted_x, x0-(threshold-x0))
-else:
-    print('Low values are unusual:')
-    N_normal = 2*(N-N_mode)
-    threshold = sorted_x[-N_normal]
-    contamination = N - np.searchsorted(sorted_x, x0 + (x0-threshold))
-print(f'{N_normal}/{N} "normal" values (threshold = {threshold:.3f}; contamination = {contamination}/{N - N_normal})')
+    # %% Identify outliers
 
-# %% Plot figure
+    N_mode = np.searchsorted(sorted_x, x0)
+    # N_normal = 2*N_mode
+    M_mode = cumulative_mass[N_mode]
+    M_normal = 2*M_mode
+    M_total = cumulative_mass[-1]
+    if M_normal < M_total:
+        print('High values are unusual:')
+        N_normal = np.searchsorted(cumulative_mass, M_normal)
+        threshold = sorted_x[N_normal]
+        contamination = np.interp(x0-(threshold-x0), sorted_x, cumulative_mass)
+    else:
+        print('Low values are unusual:')
+        # N_normal = 2*(N-N_mode)
+        M_normal = 2 * (M_total-M_mode)
+        N_active = np.searchsorted(cumulative_mass, M_total-M_normal)
+        threshold = sorted_x[N_active]
+        contamination = N - np.searchsorted(sorted_x, x0 + (x0-threshold))
+        contamination = M_total - np.interp(x0 + (x0-threshold), sorted_x, cumulative_mass)
+    print(f'{N_normal}/{N} "normal" values (threshold = {threshold:.3f}; contamination = {contamination}/{M_total - M_normal})')
 
-# plt.ion()
-fig, ax = plt.subplots()
-ax.set_title('{} - {} points'.format(distribution, N))
-ax.set_xlabel('x')
-ax.set_ylabel('probability')
+    # %% Plot figure
 
-ax.plot(sorted_x, true_density, 'k--')
-ax.axvline(true_mode, c='k', ls='--', label='True solution: {:.3f}'.format(true_mode))
+    # plt.ion()
+    fig, ax = plt.subplots()
+    ax.set_title('{} - {} points'.format(distribution, N))
+    ax.set_xlabel('x')
+    ax.set_ylabel('probability')
 
-xx, den = adaptive_histogram(x)
-adaptive_peak = xx[np.argmax(den)]
-ax.plot(xx, den, 'b-')
-ax.axvline(adaptive_peak, c='b', ls=':', label='Adaptive histogram: {:.3f}'.format(adaptive_peak))
+    ax.plot(sorted_x, true_density, 'k--')
+    # ax.axvline(true_mode, c='k', ls='--', label='True solution: {:.3f}'.format(true_mode))
 
-ax.plot(x_fit, polynomial_fit, 'r.')
-ax.axvline(x0, c='r', ls='-.', label='Legendre fit: {:.3f}'.format(x0))
-ax.axvline(threshold, c='r', ls='-', label='threshold: {:.3f}'.format(threshold))
+    ax.hist(x, bins=np.linspace(np.min(x), np.max(x), 2+int(np.sqrt(x.size))),
+            weights=statistical_weight, density=True, alpha=.2)
 
-ax.hist(x, bins=np.linspace(np.min(x), np.max(x), 2+int(np.sqrt(x.size))), density=True, alpha=.2)
-ax.set_xlim(np.min(x), np.max(x))
-ax.set_ylim(.5/N, 1.1)
-# ax.set_yscale('log')
-ax.legend()
+    xx, den = adaptive_histogram(x, weights=statistical_weight)
+    adaptive_peak = xx[np.argmax(den)]
+    ax.plot(xx, den, 'b-')
+    ax.axvline(adaptive_peak, c='b', ls=':', label='Adaptive histogram: {:.3f}'.format(adaptive_peak))
 
-plt.show()
+    ax.plot(x_fit, polynomial_fit, 'r.')
+    ax.axvline(x0, c='r', ls='-.', label='Legendre fit: {:.3f}'.format(x0))
 
-# %% Bye
+    ax.plot(x0 + (x0-xx), den, 'c:')
+    ax.axvline(x0 - (threshold-x0), c='r', ls='-')
+    ax.axvline(threshold, c='r', ls='-', label='threshold: {:.3f}'.format(threshold))
 
-print("... Paranoy@ Rulz!")
+    ax.set_xlim(np.min(x), np.max(x))
+    ax.set_ylim(.5/N, 1.1)
+    # ax.set_yscale('log')
+    ax.legend()
+
+    plt.show()
+
+    # %% Bye
+
+    print("... Paranoy@ Rulz!")
